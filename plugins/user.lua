bd411a6d0b3cf555abf59e9962ef12b07a566e1c
RilCritch <rilcritch@gmail.com>
2023-04-19 17:07:35 -0500

    Added Hop

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,13 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end,
+  },
 }
